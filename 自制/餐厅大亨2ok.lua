5c4240dc1689d3d6d74b10a2411d2d891ec3e480
Xiao-Ling-Script <2806224430@qq.com>
2024-12-01 18:41:54 +0800

    更新 餐厅大亨2ok.lua

--- 自制/餐厅大亨2ok.lua
+++ 自制/餐厅大亨2ok.lua
@@ -40,8 +40,7 @@ end) -- replace "E" with your keybind
 UITextSizeConstraint.Parent = TextButton
 UITextSizeConstraint.MaxTextSize = 30
 
-local lib = loadstring(game:HttpGet"https://pastebin.com/raw/aDQ86WZA")()
-
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/flyspeed7/Only/main/UI%20%E9%80%8F%E6%98%8E%20Libe.Lua")()
 local win = lib:Window("Y&F | 餐厅大亨2",Color3.fromRGB(0, 255, 0), Enum.KeyCode.E) -- your own keybind 
 
 local tab = win:Tab("主要")
